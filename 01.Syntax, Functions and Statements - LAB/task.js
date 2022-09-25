3ad5307cca736405024fd5533cee0774748379f0
Hopki <edis.ersenov23@abv.bg>
2022-09-25 18:06:47 +0300

    Added new solutions.

---
+++ 01.Syntax, Functions and Statements - LAB/task.js
@@ -0,0 +1,8 @@
+function solve(inputOne, inputTwo, inputThree){
+    let sum = inputOne.length + inputTwo.length + inputThree.length;
+    let averageSum = Math.floor(sum / 3);
+    console.log(sum);
+    console.log(averageSum);
+}
+
+solve('chocolate', 'ice cream', 'cake');
